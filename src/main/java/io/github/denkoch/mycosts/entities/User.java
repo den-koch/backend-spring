1371a694512cbf56cb115fa88bce4fd8c2a7a284
Denys Kochetov <125379298+den-koch@users.noreply.github.com>
2023-11-05 00:34:18 +0200

    Add user entity

---
+++ src/main/java/io/github/denkoch/mycosts/entities/User.java
@@ -0,0 +1,16 @@
+package io.github.denkoch.mycosts.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Integer id;
+    private String name;
+    private String lastname;
+    private String email;
+    private String password;
+}
